7d7e52d14467856fcee7ef61aab2b35d9c62a74d
Pop Aron <fezfamiliar@yahoo.com>
2023-05-13 12:06:45 +0300

    stylings

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,14 +15,22 @@
         </div>
     </div>
 
-    <div class="py-12">
+    <div class="grid grid-cols-3 gap-4 mx-auto" style="width:1200px;">
         @foreach ($movies as $key => $movie)
-            
-            <p>Title: {{ $movie['title'] ?? ''}}</p>
-            <p>Year: {{ $movie['release_date'] ?? ''}}</p>
-            <p>vote: {{ $movie['vote_count'] ?? ''}}</p>
-            <p>Description: {{ $movie['overview'] ?? ''}}</p>
-            <img src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] ?? ''}}" alt="movie_poster">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 w-400">
+                <img src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] ?? ''}}" alt="movie_poster" style="height:528px;">
+                <div class="px-6 py-4 overflow-hidden" style="height:200px;">
+                  <div class="font-bold text-xl mb-2">{{ $movie['title'] ?? $movie['name']}}</div>
+                  <p class="text-gray-700 text-base">
+                    {{ $movie['overview'] ?? ''}}
+                  </p>
+                </div>
+                <div class="px-6 pt-4 pb-2 text-center">
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ isset($movie['release_date']) ? substr($movie['release_date'], 0, 4) : '2017'}}</span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ $movie['vote_count'] ?? ''}}</span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ $movie['vote_average'] ?? ''}}/10</span>
+                </div>
+            </div>
         @endforeach
     </div>
 </x-app-layout>
